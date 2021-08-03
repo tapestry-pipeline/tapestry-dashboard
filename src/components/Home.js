2b66c1574a250b3c21a606720be689dcef2b6fa1
Katherine Beck <katherine.murphy.beck@gmail.com>
2021-08-02 22:22:14 -0700

    updated homepage margin

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ function Home() {
     return (
         <>
           <div className="relative w-full h-full flex-overflow">
-            <div className="relative px-4 xl:px-0  container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 pt-16 lg:pt-36 gap-8">
+            <div className="relative px-10 ml-5 xl:px-0  container mx-auto grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 pt-16 lg:pt-36 gap-8">
               <div>
                 <h1 className="flex-shrink text-6xl w-1/2 xl:w-full xl:text-8xl text-grouparooblue font-sans f-f-l">Ingest. Transform. Sync.</h1>
               </div>
